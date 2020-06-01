0c920b267172f4d9d92432c38b0e7dbf79c61ee5
klimaleksus <klimaleksus@gmail.com>
2020-06-01 17:48:28 +0500

    Update _myawesomeexport_.h

--- Telegram/SourceFiles/_myawesomeexport_.h
+++ Telegram/SourceFiles/_myawesomeexport_.h
@@ -2,22 +2,4 @@
 void _myawesomeexport_ChatSelected(void* data);
 void _myawesomeexport_ExportRequested(void);
 
-#ifndef _myawesomeexport_h_
-#define _myawesomeexport_h_
-
-#include "window/window_peer_menu.h"
-#include "data/data_peer.h"
-
-PeerData* _myawesomeexport_chatselected_ = NULL;
-
-void _myawesomeexport_ChatSelected(void* data){
-  _myawesomeexport_chatselected_ = (PeerData*)data;
-};
-
-void _myawesomeexport_ExportRequested(void){
-  if(!_myawesomeexport_chatselected_)
-    return;
-  Window::PeerMenuExportChat(_myawesomeexport_chatselected_);
-};
-
-#endif
+//EOF
